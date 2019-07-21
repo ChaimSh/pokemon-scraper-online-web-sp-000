20fba6895f5b92d76241d508f03919b286a7e807
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-21 22:11:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,12 +23,7 @@ attr_accessor :id, :name, :type, :db
   #   end
   def self.find(num, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
-    new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
-  #  new_pokemon.hp = pokemon[0][3]
-    return new_pokemon
+  Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
   end
 
 end
